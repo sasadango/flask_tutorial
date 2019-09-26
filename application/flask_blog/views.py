ab02da5fac5bcf4d63d5ad490cea98a27c3bc310
sasadango <t20th_century_turn@yahoo.co.jp>
2019-09-26 23:58:27 +0900

    0926

--- application/flask_blog/views.py
+++ application/flask_blog/views.py
@@ -1,8 +1,16 @@
 # flask_blog/views.py
 
+from flask import (
+    request,
+    redirect,
+    url_for,
+    render_template,
+    flash,
+    session
+)
 from flask_blog import app
 
 
 @app.route('/')
 def show_entries():
-    return "Hello World!"
+    return render_template('entries/index.html')
