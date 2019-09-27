07991c40accdd402603b1da69b99c60c7f8d5dfb
hatsuhiko <tanabe.hatsuhiko@showcase-tv.com>
2019-09-27 19:02:05 +0900

    flash

--- application/flask_blog/config.py
+++ application/flask_blog/config.py
@@ -1,5 +1,6 @@
 # flask_blog/config.py
 
 DEBUG = True
+SECRET_KEY = 'secret key'
 USERNAME = 'john'
 PASSWORD = 'due123'
--- application/flask_blog/views.py
+++ application/flask_blog/views.py
@@ -13,21 +13,28 @@ from flask_blog import app
 
 @app.route('/')
 def show_entries():
+    if not session.get('logged_in'):
+        return redirect('/login')
     return render_template('entries/index.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    error = None
     if request.method == 'POST':
         if request.form['username'] != app.config['USERNAME']:
-            print('ユーザー名が異なります')
+            flash('ユーザー名が異なります')
         elif request.form['password'] != app.config['PASSWORD']:
-            print('パスワードが異なります')
+            flash('パスワードが異なります')
         else:
+            session['logged_in'] = True
+            flash('ログインしました')
             return redirect('/')
     return render_template('login.html')
 
 
 @app.route('/logout')
 def logout():
+    session.pop('logged_in', None)
+    flash('ログアウトしました')
     return redirect('/')
