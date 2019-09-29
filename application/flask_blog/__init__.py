67a4e303d990399aff9b823820cf0d8a241b8052
sasadango <t20th_century_turn@yahoo.co.jp>
2019-09-29 17:42:51 +0900

    test

--- application/flask_blog/__init__.py
+++ application/flask_blog/__init__.py
@@ -10,7 +10,8 @@ app.config.from_object('flask_blog.config')
 db = SQLAlchemy(app)
 
 
-from flask_blog.views import (
-    views,
-    entries
- )  # NOQA
+from flask_blog.views.entries import entry  # NOQA
+app.register_blueprint(entry, url_prefix='/useers')
+
+
+from flask_blog.views import views  # NOQA
