dedd1daeb0d0af12e0df08eadfd5d613c9f0ffbe
sasadango <t20th_century_turn@yahoo.co.jp>
2019-09-29 11:49:13 +0900

    decorator

--- application/flask_blog/views/views.py
+++ application/flask_blog/views/views.py
@@ -9,6 +9,16 @@ from flask import (
     session
 )
 from flask_blog import app
+from functools import wraps
+
+
+def login_required(view):
+    @wraps(view)
+    def inner(*args, **kwargs):
+        if not session.get('logged_in'):
+            return redirect(url_for('login'))
+        return view(*args, **kwargs)
+    return inner
 
 
 @app.route('/login', methods=['GET', 'POST'])
