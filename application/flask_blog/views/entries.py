1944915f4a8d850d893efead7cb59ea664c05a17
sasadango <t20th_century_turn@yahoo.co.jp>
2019-09-29 10:09:14 +0900

    update

--- application/flask_blog/views/entries.py
+++ application/flask_blog/views/entries.py
@@ -38,3 +38,32 @@ def new_entry():
     if not session.get('logged_in'):
         return redirect(url_for('login'))
     return render_template('entries/new.html')
+
+
+@app.route('/entries/<int:id>', methods=['GET'])
+def show_entry(id):
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    entry = Entry.query.get(id)
+    return render_template('entries/show.html', entry=entry)
+
+
+@app.route('/entries/<int:id>/edit', methods=['GET'])
+def edit_entry(id):
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    entry = Entry.query.get(id)
+    return render_template('entries/edit.html', entry=entry)
+
+
+@app.route('/entries/<int:id>/update', methods=['POST'])
+def update_entry(id):
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    entry = Entry.query.get(id)
+    entry.title = request.form['title']
+    entry.text = request.form['text']
+    db.session.merge(entry)
+    db.session.commit()
+    flash('記事が更新されました')
+    return redirect(url_for('show_entries'))
